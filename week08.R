d9d35713963c9b1cfac137e72f8b9ba0855e37d2
mashuzza <masha.soboleva89@gmail.com>
2020-03-08 10:32:23 -0400

    - small changes in code

--- week08.R
+++ week08.R
@@ -10,6 +10,21 @@ head(measles)
 
 ## some schools have 0 enrollment?!
 
-measles%>%filter(enroll == 0) %>%print(n = Inf)
+measles%>%filter(enroll == 0| mmr == -1) %>%print(n = 20)
 
 summary(as.factor(measles$year))
+
+
+tx_county<-map_data("county") %>% 
+  ##filter(region=='texas')%>%
+  mutate(state_county = str_c(region, subregion, sep = "_"))
+
+## use county map
+ggplot() + 
+  geom_polygon(data = tx_county,
+               mapping = aes(x = long, y = lat, group = group),
+               size = 0.3, 
+               fill = "#f5f5f2", 
+               color = "darkgray") +
+  coord_fixed(1.3) +
+  theme_bw()
